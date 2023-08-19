901c48b038ca6adccceae4051ade492c5e21a4c1
anuj7862 <anujtiwarimnnit@gmail.com>
2023-08-20 02:21:54 +0530

    Test cases for Movie details page

---
+++ src/pages/MovieDetails/MovieDetails.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import MovieDetails from './MovieDetails';
+
+test('must render movie details correctly with all needed element', () => {
+    const props = {
+        id: 12,
+    };
+
+    const {getByText, getByAltText} = render(<MovieDetails {...props}/>);
+
+    const imageTag = getByAltText('');
+    const ratingDiv = getByText('(');
+    const castDiv = getByText('cast:');
+    const descriptionDiv = getByText('Description:');
+
+    expect(imageTag).toBeInTheDocument();
+    expect(ratingDiv).toBeInTheDocument();
+    expect(castDiv).toBeInTheDocument();
+    expect(descriptionDiv).toBeInTheDocument();
+
+});
