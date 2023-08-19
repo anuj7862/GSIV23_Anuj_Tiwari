1adcf5b23b192246c4b803f69a2ef7a880793c41
anuj7862 <anujtiwarimnnit@gmail.com>
2023-08-19 21:34:16 +0530

    MovieCard component
    Implementing Movie Card component as per test cases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-
+//import './App.css';
+import './App.sass';
+import MovieCard from './component/MovieCard/MovieCard';
 function App() {
   return (
     <div className="App">
-      
+      <MovieCard 
+        id={2} title="movie1" preview="https://image.tmdb.org/t/p/w200/bHH5jiumG6QXpvKfUEN2QsaBVV.jpg"
+         rating={2} description="Desc.c...c."
+       />
     </div>
   );
 }
--- src/component/MovieCard/MovieCard.js
+++ src/component/MovieCard/MovieCard.js
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import './MovieCard.sass';
 export default function MovieCard(props) {
-  return (
-    <div>MovieCard</div>
-  )
+    return (
+        <div className='movie-card' key={props.id}>
+            <img src={props?.preview} alt="" className="preview" />
+            <div className='info'>
+                <p className='title'>{props?.title}</p>
+                <p className='rating'>{props?.rating}</p>
+            </div>
+            <p className='description'>{props?.description}</p>
+        </div>
+    )
 };
